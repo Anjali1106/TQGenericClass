8403125f24c2487bd7f5602604967b296edf5b98
Anjali <anjali123@gmail.com>
2024-11-19 19:28:14 +0530

    Add project files.

---
+++ GenericClass/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace GenericClass
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string price { get; set; }
+    }
+}
